af9ba66d40842d79cb776b008bb392314f7e7c89
korg-pa <salonsimsim644i@gmail.com>
2026-03-22 21:02:21 +0300

    Add translations for multiple languages

---
+++ translations.py
@@ -0,0 +1,22 @@
+translations = {
+    'az': {
+        'greeting': 'Salam',
+        'farewell': 'Sağ olun'
+    },
+    'tr': {
+        'greeting': 'Merhaba',
+        'farewell': 'Hoşça kal'
+    },
+    'bs': {
+        'greeting': 'Zdravo',
+        'farewell': 'Doviđenja'
+    },
+    'ru': {
+        'greeting': 'Здравствуйте',
+        'farewell': 'До свидания'
+    },
+    'en': {
+        'greeting': 'Hello',
+        'farewell': 'Goodbye'
+    }
+}
