c76cfb4dcda4e8009e89da57130a3f7853680db2
korg-pa <salonsimsim644i@gmail.com>
2026-03-22 21:00:38 +0300

    Create multisample_generator.py with MultisampleGenerator class

---
+++ multisample_generator.py
@@ -0,0 +1,7 @@
+class MultisampleGenerator:
+    def __init__(self, samples):
+        self.samples = samples
+
+    def generate(self):
+        # Implement the logic to generate multisamples from the provided samples
+        pass
